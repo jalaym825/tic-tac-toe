e5b053eb42e34eae5779bd874e7315662b045e6c
Jalay Movaliya <jalaym123@gmail.com>
2023-05-06 06:59:50 +0530

    handle: duplicate inputs, corner cases

--- Player.h
+++ Player.h
@@ -1,60 +1,67 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Player
 {
-public:
     string name;
     char ch;
     static int x;
     vector<int> v;
-    void get()
+
+public:
+    void get();
+    void set(int y);
+    bool isWinner();
+    friend class TTT;
+};
+
+int Player::x = 1;
+
+bool Player::isWinner()
+{
+    sort(v.begin(), v.end());
+    vector<vector<int>> combs = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+        {1, 4, 7},
+        {2, 5, 8},
+        {3, 6, 9},
+        {1, 5, 9},
+        {3, 5, 7}};
+    for (int i = 0; i < v.size() - 2; i++)
     {
-        cout << "Enter player " << x << " name: ";
-        cin >> name;
-        if (x == 1)
+        for (int j = i + 1; j < v.size() - 1; j++)
         {
-            cout << "Take X or 0:";
-            cin >> ch;
-        }
-        else
-            ch = (ch == 'X' ? '0' : 'X');
-        x++;
-    }
-    void set(int y)
-    {
-        v.push_back(y);
-    }
-    bool isWinner()
-    {
-        sort(v.begin(), v.end());
-        vector<vector<int>> combs = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9},
-            {1, 4, 7},
-            {2, 5, 8},
-            {3, 6, 9},
-            {1, 5, 9},
-            {3, 5, 7}};
-        for (int i = 0; i < v.size() - 2; i++)
-        {
-            for (int j = i + 1; j < v.size() - 1; j++)
+            for (int k = j + 1; k < v.size(); k++)
             {
-                for (int k = j + 1; k < v.size(); k++)
-                {
-                    vector<int> v1 = {v[i], v[j], v[k]};
-                    for (auto it : combs)
-                    {
-                        if (v1 == it)
-                        {
-                            return true;
-                        }
-                    }
-                }
+                vector<int> v1 = {v[i], v[j], v[k]};
+                for (auto it : combs)
+                    if (v1 == it)
+                        return true;
             }
         }
-        return false;
     }
-};
-int Player::x = 1;
+    return false;
+}
+
+char ch1;
+void Player::get()
+{
+    cout << "Enter player " << x << " name: ";
+    cin >> name;
+    if (x == 1)
+    {
+        cout << "Take X or 0:";
+        cin >> ch;
+        ch1 = ch;
+    }
+    else
+        ch = (ch1 == 'X' ? '0' : 'X');
+    x++;
+}
+
+void Player::set(int y)
+{
+    v.push_back(y);
+}
--- TTT.h
+++ TTT.h
@@ -4,9 +4,8 @@ using namespace std;
 class TTT
 {
 private:
-
-public:
     vector<vector<char>> v = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+    set<int> s;
     map<int, pair<int, int>> m1 = {
         {1, {0, 0}},
         {2, {0, 1}},
@@ -18,23 +17,59 @@ public:
         {8, {2, 1}},
         {9, {2, 2}},
     };
+    Player players[2];
+
+public:
     TTT()
     {
         cout << "HERE THE GAME BEGINS:" << endl;
+        players[0].get();
+        players[1].get();
     }
     void display();
+    bool NextTurn(int);
 };
 
 void TTT::display()
 {
-    cout << "HERE THE GAME BEGINS:" << endl;
     for (int i = 0; i < v.size(); i++)
     {
         for (int j = 0; j < v[i].size(); j++)
-        {
             cout << v[i][j] << ((j != 2) ? " | " : "");
-        }
+
         cout << "\n"
              << ((i != 2) ? "---------" : "") << "\n";
+    }
+    cout << endl;
+}
+
+bool TTT::NextTurn(int p)
+{
+    int in;
+    while (true)
+    {
+        cout << players[p].name << "'s Turn: ";
+        cin >> in;
+        if (s.count(in) == 1)
+        {
+            cout << "Your input already taken in this game!" << endl;
+            continue;
+        }
+        if (in <= 0 || in > 9)
+        {
+            cout << "Your input is out of range!" << endl;
+            continue;
+        }
+
+        v[m1[in].first][m1[in].second] = players[p].ch;
+        players[p].set(in);
+        s.insert(in);
+        display();
+        if (players[p].v.size() >= 3 && players[p].isWinner())
+        {
+            cout << players[p].name << " won the game." << endl;
+            return false;
+        }
+        return true;
     }
 }
